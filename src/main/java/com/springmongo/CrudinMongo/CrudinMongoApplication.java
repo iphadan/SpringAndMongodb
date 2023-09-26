9c198e4320c96e3c476a32e195c0bbb5760ae9b9
iphadan <ephadaniel177@gmail.com>
2023-09-26 09:53:05 +0300

    initial

---
+++ src/main/java/com/springmongo/CrudinMongo/CrudinMongoApplication.java
@@ -0,0 +1,13 @@
+package com.springmongo.CrudinMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudinMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudinMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springmongo/CrudinMongo/CrudinMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmongo.CrudinMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudinMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
